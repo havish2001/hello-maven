4e517304d0e3456bed9df0a7c383818687e614d7
Havish Vadlamani <54392756+havish2001@users.noreply.github.com>
2026-04-05 12:00:32 +0530

    Replace 'world' with 'Good' in App.java

--- src/main/java/Maven/App.java
+++ src/main/java/Maven/App.java
@@ -7,6 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 	System.out.println("Maven");
-	System.out("world");
+	System.out.println("Good");
     }
 }
